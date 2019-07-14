5ac1fff9b3aba7932e5a70c5f6b88c084f9af5bd
Gokul <muthu.gokul@gmail.com>
2019-07-14 22:14:53 +0100

    Implented filter and sort for users

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe("workspace-project App", () => {
 
   it("should display welcome message", () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual("Welcome to Task Manager");
+    expect(page.getParagraphText()).toEqual("Welcome to Project Manager");
   });
 });
--- src/app/user/view/view-user.component.ts
+++ src/app/user/view/view-user.component.ts
@@ -1,22 +1,43 @@
-import { Component, Input, Output, EventEmitter } from "@angular/core";
+import { Component, Input, Output, EventEmitter, OnInit } from "@angular/core";
 import { User } from "../models/user.model";
 import { ROUTES } from "src/app/routes";
 import { Router, ActivatedRoute } from "@angular/router";
+import { FormGroup, FormControl, AbstractControl } from "@angular/forms";
 
 @Component({
     selector: "view-user",
     templateUrl: "./view-user.component.html"
 })
 
-export class ViewUserComponent {
+export class ViewUserComponent implements OnInit {
   @Input() users: User[];
 
   @Output() deleteUser = new EventEmitter<number>();
   @Output() editUser = new EventEmitter<User>();
 
-  constructor(
-    private router: Router,
-    private route: ActivatedRoute) {
+  usersToDisplay: User[] = [];
+  sortBy = "firstname";
+  searchText: string;
+
+  searchForm: FormGroup;
+
+  constructor() {
+    this.searchForm = new FormGroup({
+      search: new FormControl()
+    });
+  }
+
+  get searchControl(): AbstractControl {
+    return this.searchForm.get("search") as AbstractControl;
+  }
+
+  ngOnInit(): void {
+    this.getUsers();
+
+    this.searchControl.valueChanges.subscribe(x => {
+      this.searchText = x;
+      this.getUsers();
+    });
   }
 
   delete(id: number): void {
@@ -25,6 +46,43 @@ export class ViewUserComponent {
 
   edit(user: User): void {
     this.editUser.emit(user.clone());
-    // this.router.navigate([ROUTES.UPDATE, id], { relativeTo: this.route });
+  }
+
+  setSortBy(sortBy: string): void {
+    this.sortBy = sortBy;
+    this.getUsers();
+  }
+
+  private getUsers(): void {
+    if (this.users) {
+      const filteredUsers = this.filterUsers();
+      this.usersToDisplay = this.sortUser(filteredUsers);
+    }
+  }
+
+  private filterUsers(): User[] {
+    let filteredUsers = this.users;
+
+    if (this.searchText && this.searchText !== "") {
+      filteredUsers = this.users.filter(x => x.firstName.toLowerCase().includes(this.searchText.trim().toLowerCase())
+        || x.lastName.toLowerCase().includes(this.searchText.trim().toLowerCase()));
+    }
+
+    return filteredUsers;
+  }
+
+  private sortUser(filteredUsers: User[]): User[] {
+    if (filteredUsers) {
+      switch (this.sortBy) {
+        case "firstname":
+          return filteredUsers.sort((x, y) => (x.firstName < y.firstName ? -1 : 1));
+        case "lastname":
+            return filteredUsers.sort((x, y) => (x.lastName < y.lastName ? -1 : 1));
+        case "id":
+          return filteredUsers.sort((x, y) => (x.employeeId - y.employeeId));
+        default:
+          return filteredUsers;
+      }
+    }
   }
 }
